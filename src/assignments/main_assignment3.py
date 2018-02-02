7330f68e35c6df949cc9fcc9b323b062d061d6f4
ZachariahClymerAcc <35586553+ZachariahClymerAcc@users.noreply.github.com>
2018-02-01 22:20:13 -0600

    Add files via upload

---
+++ src/assignments/main_assignment3.py
@@ -0,0 +1,38 @@
+from src.assignments.assignment3 import decimal_to_binary
+from src.assignments.assignment3 import sum_square_of_number
+from src.assignments.assignment3 import is_prime
+from src.assignments.assignment3 import list_of_primes
+
+#this will show you how you can use the functions from a main Python file
+
+#write code to prompt user for a number save to a num variable,
+#call decimal_to_binary with argument of num variable save return value to result variable,
+#print result variable
+
+num = int(input("Enter a number: "))#prompt user for a number from keyboard 
+result = decimal_to_binary(num) #call decimal_to_binary function with argument of num(this is a variable) and save return value of function to result
+print(result)#print the result variable
+
+#QUESTION 1
+#see my example above
+#prompt user for a number from keyboard save to a num variable,
+#call sum_square_of_number with argument of
+#num, save return value to result variable, print result variable
+
+
+#QUESTION 2
+#prompt user for a number from keyboard create a variable num
+#code to call is_prime with argument of num variable
+#save return value to result variable
+#print result variable
+
+
+
+#QUESTION 3
+#prompt user for a number from keyboard create a variable num
+#code to call list_of_primes with argument of num variable
+#save return value to result variable
+#print result variable
+
+
+
