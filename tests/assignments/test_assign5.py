831967a7818af8025a2ddbd716093ce0200773c1
ZachariahClymerAcc <35586553+ZachariahClymerAcc@users.noreply.github.com>
2018-02-15 21:31:11 -0600

    Add files via upload

---
+++ tests/assignments/test_assign5.py
@@ -0,0 +1,20 @@
+import unittest
+from assignment5 import recursive_decimal_binary
+
+#write import for decimal to binary function
+
+
+class Test_Assign4(unittest.TestCase):
+
+    def test_sample_one(self):
+        self.assertEqual(1,1)
+
+    def test_RDB_w_base_case(self):
+        self.assertEqual('00000000', recursive_decimal_binary(0, 7))
+
+    def test_RDB_w_value_65(self):
+        self.assertEqual('01000001', recursive_decimal_binary(65, 7))
+
+    #write test cases with arguments 85 and 63 for recursive_decimal_binary function
+
+unittest.main(verbosity=2)
