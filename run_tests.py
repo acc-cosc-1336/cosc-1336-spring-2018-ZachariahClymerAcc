26a8b5e869ecdda996183b8d949e675b85fbacae
ZachariahClymerAcc <35586553+ZachariahClymerAcc@users.noreply.github.com>
2018-04-24 22:28:22 -0500

    Delete run_tests.py

--- run_tests.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-from tests.assignment import test_assignmentk7
-#import test_homework7
-
-suite = unittest.TestLoader().loadTestsFromModule(test_assignment7)
-unittest.TextTestRunner(verbosity=2).run(suite)
