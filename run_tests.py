57a7c98234f89a735639c87340103f83a1002828
ZachariahClymerAcc <35586553+ZachariahClymerAcc@users.noreply.github.com>
2018-05-10 14:09:14 -0500

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from tests.assignment import test_assignment10
+from tests.assignment import test_assignment11
 
-suite = unittest.TestLoader().loadTestsFromModule(test_assignment10)
+suite = unittest.TestLoader().loadTestsFromModule(test_assignment11)
 unittest.TextTestRunner(verbosity=2).run(suite)
